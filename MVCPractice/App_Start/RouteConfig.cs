4f0148bca0341570f49868e333ac365f9d463ff9
Jonathan Ho <jonathanh@adept.com.au>
2019-11-29 17:03:28 +1100

    Commit on Second day.

--- MVCPractice/App_Start/RouteConfig.cs
+++ MVCPractice/App_Start/RouteConfig.cs
@@ -18,6 +18,17 @@ namespace MVCPractice
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Index",
+                url: "{controller}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Hello",
+                url: "{controller}/{action}/{name}/{id}"
+            );
         }
     }
 }
